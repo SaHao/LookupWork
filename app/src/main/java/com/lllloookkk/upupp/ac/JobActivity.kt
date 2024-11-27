3812055921385acc43d9fca1a88ea911f8dc2875
Chang <421988253@qq.com>
2024-11-27 18:08:26 +0800

    111

---
+++ app/src/main/java/com/lllloookkk/upupp/ac/JobActivity.kt
@@ -0,0 +1,21 @@
+package com.lllloookkk.upupp.ac
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lllloookkk.upupp.R
+
+class JobActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_job)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
